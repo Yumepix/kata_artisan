ebeaff6c214fc604d324f392bfb3e86757a492a6
Spark009 <pyxelcode@pm.me>
2022-02-17 12:56:37 +0100

    kata romain ok

--- js/src/roman.js
+++ js/src/roman.js
@@ -1,6 +1,38 @@
 class Roman {
-    decode(romanNumber){
-        return -1;
+    decode(romanNumber) {
+        let result = 0;
+        let previous_char = 0;
+        for (let i = 0; i < romanNumber.length; i++){
+            const current_char = this.decodeSingleChar(romanNumber.charAt(i));
+
+            if (current_char > previous_char) {
+                result += (current_char - previous_char) - previous_char;
+            }else{
+                result += current_char;
+            }
+
+            previous_char = current_char;
+        }
+        return result;
+    }
+
+    decodeSingleChar(romanNumber) {
+        switch (romanNumber) {
+            case 'I':
+                return 1
+            case 'V':
+                return 5
+            case 'X':
+                return 10
+            case 'L':
+                return 50
+            case 'C':
+                return 100
+            case 'D':
+                return 500
+            case 'M':
+                return 1000
+        }
     }
 }
 
--- js/test/roman.test.js
+++ js/test/roman.test.js
@@ -7,5 +7,52 @@ describe('Roman', function() {
         roman = new Roman();
     });
 
-    it('should decode a roman number into integer');
+    it('should decode CDXLVIII into integer 448', function() {
+        assert.equal(roman.decode('CDXLVIII'),448);
+    });
+
+    it('should decode MCMXCVIII into integer 1998', function() {
+        assert.equal(roman.decode('MCMXCVIII'),1998);
+    });
+
+    it('should decode I into integer 1', function() {
+        assert.equal(roman.decode('I'),1);
+    });
+
+    it('should decode V into integer 5', function() {
+        assert.equal(roman.decode('V'),5);
+    });
+
+    it('should decode L into integer 50', function() {
+        assert.equal(roman.decode('L'),50);
+    });
+
+    it('should decode C into integer 100', function() {
+        assert.equal(roman.decode('C'),100);
+    });
+
+    it('should decode D into integer 500', function() {
+        assert.equal(roman.decode('D'),500);
+    });
+
+    it('should decode M into integer 1000', function() {
+        assert.equal(roman.decode('M'),1000);
+    });
+
+    it('should sum II and return 2', function() {
+        assert.equal(roman.decode('II'),2);
+    });
+
+    it('should substract I of V and return 4', function() {
+        assert.equal(roman.decode('IV'),4);
+    });
+
+    it('should decode DCLXVI and return 666', function() {
+        assert.equal(roman.decode('DCLXVI'),666);
+    });
+
+    it('should decode MMMMMMMMMMMMDXLIX and return 12549', function() {
+        assert.equal(roman.decode('MMMMMMMMMMMMDXLIX'),12549);
+    });
+
 });
